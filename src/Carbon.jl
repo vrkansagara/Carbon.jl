be90c6b27aa6e8acf8b58ed2660a7f74c47ebf8a
Vallabh Kansagara <vrkansagara@gmail.com>
2023-10-15 22:10:08 +0530

    fixing file path

--- src/Carbon.jl
+++ src/Carbon.jl
@@ -4,8 +4,8 @@ Loads dependencies and bootstraps a Carbon.
 
 module Carbon
 
-include(joinpath(pwd(), "src/utilities.jl"))
-include(joinpath(pwd(), "src/functions.jl"))
+include("utilities.jl")
+include("functions.jl")
 
 # export, using, import statements are usually here; we discuss these below
 export greet, version, now
