8c570c09c0183408e27957c94883d98a665eb45c
Vallabh Kansagara <vrkansagara@gmail.com>
2023-10-15 19:32:26 +0530

    fix the unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Carbon
 using Test
 
 @testset "Carbon.version" begin
-    @test Carbon.version() == "1.0.1"
-    @test Carbon.version() != "1.0.2"
+    @test Carbon.version() == "1.0.0"
+    @test Carbon.version() != "1.0.1"
 end
 
 @testset "Carbon.jl" begin
